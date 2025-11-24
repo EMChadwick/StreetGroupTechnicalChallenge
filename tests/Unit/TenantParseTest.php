1e2706095841d99caf75c9fff3a261d528d8a3df
Ed Chadwick <edwardmchadwick@gmail.com>
2025-11-24 17:15:09 +0000

    feat: refactor tests to use a DataProvider

--- tests/Unit/TenantParseTest.php
+++ tests/Unit/TenantParseTest.php
@@ -3,7 +3,9 @@
 namespace Tests\Unit;
 
 use App\Services\HomeownerParser;
-use PHPUnit\Framework\TestCase;
+use App\Enums\Title;
+use Tests\TestCase;
+use PHPUnit\Framework\Attributes\DataProvider;
 
 class TenantParseTest extends TestCase
 {
@@ -13,122 +15,42 @@ class TenantParseTest extends TestCase
     {
         parent::setUp();
 
-        $this->parser = app(HomeownerParser::class);
+        $this->parser = new HomeownerParser(Title::values());
     }
 
-    /**
-     * A basic unit test example.
-     */
-    public function test_single_name_processes_correctly(): void
+
+    #[Test]
+    #[DataProvider('homeownerCases')]
+    public function test_parser_can_handle_examples(string $input, array $expected): void
     {
-        $result = $this->parser->parseEntry('Mr John Smith');
-
-        $expectedResult = [
-            [
-                'title' => 'Mr',
-                'first_name' => 'John',
-                'last_name' => 'Smith',
-                'initial' => null,
-            ],
-        ];
-
-        $this->assertEquals($result, $expectedResult);
+        $result = $this->parser->parseEntry($input);
+        $this->assertEquals($expected, $result);
     }
 
-    public function test_initialed_name_processes_correctly(): void
+    public static function homeownerCases(): array
     {
-        $result = $this->parser->parseEntry('Ms K Mulgrew');
-
-        $expectedResult = [
-            [
-                'title' => 'Ms',
-                'first_name' => null,
-                'last_name' => 'Mulgrew',
-                'initial' => 'K',
-            ],
+        return [
+            ["Mr John Smith", [
+                ['title'=>'Mr', 'first_name'=>'John','last_name'=>'Smith','initial'=>null]
+            ]],
+            ["Ms K Mulgrew", [
+                ['title'=>'Ms','first_name'=>null,'last_name'=>'Mulgrew','initial'=>'K'],
+            ]],
+            ["Mr Daniel Day-Lewis", [
+                ['title'=>'Mr','first_name'=>'Daniel','last_name'=>'Day-Lewis','initial'=>null]
+            ]],
+            ["Mr and Mrs Smith", [
+                ['title'=>'Mr','first_name'=>null,'last_name'=>'Smith','initial'=>null],
+                ['title'=>'Mrs','first_name'=>null,'last_name'=>'Smith','initial'=>null]
+            ]],
+            ["Dr Gabriella Boudreau and Mr Andrew Ryan", [
+                ['title'=>'Dr','first_name'=>'Gabriella','last_name'=>'Boudreau','initial'=>null],
+                ['title'=>'Mr','first_name'=>'Andrew','last_name'=>'Ryan','initial'=>null]
+            ]],
+            ["Mr and Mrs Gavin Stacey", [
+                ['title'=>'Mr','first_name'=>'Gavin','last_name'=>'Stacey','initial'=>null],
+                ['title'=>'Mrs','first_name'=>null,'last_name'=>'Stacey','initial'=>null]
+            ]],
         ];
-
-        $this->assertEquals($result, $expectedResult);
-    }
-
-    public function test_hyphenated_name_processes_correctly(): void
-    {
-        $result = $this->parser->parseEntry('Mr Daniel Day-Lewis');
-        $expectedResult = [
-            [
-                'title' => 'Mr',
-                'first_name' => 'Daniel',
-                'last_name' => 'Day-Lewis',
-                'initial' => null,
-            ],
-        ];
-
-        $this->assertEquals($result, $expectedResult);
-    }
-
-    public function test_joint_name_processes_correctly(): void
-    {
-        $result = $this->parser->parseEntry('Mr and Mrs Smith');
-
-        $expectedResult = [
-            [
-                'title' => 'Mr',
-                'first_name' => null,
-                'last_name' => 'Smith',
-                'initial' => null,
-            ],
-            [
-                'title' => 'Mrs',
-                'first_name' => null,
-                'last_name' => 'Smith',
-                'initial' => null,
-            ],
-        ];
-
-        $this->assertEquals($result, $expectedResult);
-    }
-
-    public function test_multiple_full_names_process_correctly(): void
-    {
-        $result = $this->parser->parseEntry('Dr Gabriella Boudreau and Mr Andrew Ryan');
-
-        $expectedResult = [
-            [
-                'title' => 'Dr',
-                'first_name' => 'Gabriella',
-                'last_name' => 'Boudreau',
-                'initial' => null,
-            ],
-            [
-                'title' => 'Mr',
-                'first_name' => 'Andrew',
-                'last_name' => 'Ryan',
-                'initial' => null,
-            ],
-        ];
-
-        $this->assertEquals($result, $expectedResult);
-    }
-
-    public function test_joint_full_name_processes_correctly(): void
-    {
-        $result = $this->parser->parseEntry('Mr and Mrs Gavin Stacey');
-
-        $expectedResult = [
-            [
-                'title' => 'Mr',
-                'first_name' => 'Gavin',
-                'last_name' => 'Stacey',
-                'initial' => null,
-            ],
-            [
-                'title' => 'Mrs',
-                'first_name' => null,
-                'last_name' => 'Stacey',
-                'initial' => null,
-            ],
-        ];
-
-        $this->assertEquals($result, $expectedResult);
     }
 }
